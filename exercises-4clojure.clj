bd74f021a7495ad9014ce246f3ef08a217ea141e
Nic McPhee <mcphee@morris.umn.edu>
2015-09-01 12:59:43 -0500

    Created a stub for the answers file.

---
+++ exercises-4clojure.clj
@@ -0,0 +1,35 @@
+;;; Put your solutions to the assigned 4Clojure exercises here and commit
+;;; back up to your fork on Github.
+
+;;; What you enter is what whatever you'd put in the "Code which fills in the blank"
+;;; box in 4Clojure. You're welcome to include other things here (helper definitions,
+;;; scratch computations you found helpful, etc.), SO MAKE SURE YOU LABEL YOUR
+;;; "ANSWERS" clearly.
+
+;;; I've included a few examples below just to give you a sense of what I'm looking
+;;; for.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem #162: Logical falsity and true
+
+; What I pasted in the box:
+
+1
+
+; because all the given expressions returned 1, for example:
+
+(= 1 (if [] 1 0))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem #15: Double Down
+
+; What I pasted in the box:
+
+#(* 2 %)
+
+; which is an anonymous function that doubles its argument, for example:
+
+(= (#(* 2 %) 11) 22)
+
